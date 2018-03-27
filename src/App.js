88c5ca3497d7ba2713eedd162c81a7b648947319
pedro diaz <pjdaze@gmail.com>
2018-03-26 22:23:42 -0400

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import './App.css';
+
+const MainLayout = styled.div`
+
+height: 50vh; 
+
+display: flex;
+justify-content: center;
+align-items: center;
+h1 {
+  text-align: center;
+}
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+  <MainLayout><h1>PJDUX<br />React <br/> Boilerplate</h1></MainLayout>
+    );
+  }
+}
+
+export default App;
---
+++ src/Routes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    NavLink
+} from 'react-router-dom';
+
+import ScrollToTopRoute from './History';
+
+
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
+
+const Routes = (props) => (
+
+
+    <Switch>
+
+        <ScrollToTopRoute path="/" exact component={Home}/>
+        <ScrollToTopRoute path="/Home" component={Home}/>
+        <ScrollToTopRoute path="/About" exact component={About}/>
+        <ScrollToTopRoute path="/Contact" exact component={Contact}/>
+        <ScrollToTopRoute path="/Projects" exact component={Projects}/>
+    </Switch>
+
+
+
+);
+
+export default Routes;
