2bcb4e2f43347ec29b1fb349b3cab23f9dddc00e
pedro diaz <pjdaze@gmail.com>
2018-04-15 01:54:05 -0400

    stable state sort of working

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,26 +1,51 @@
 import React from "react";
 import styled from "styled-components";
+import OilList from "../OilList";
 
 const HomeWrapper = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-
+  .add-button {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
   .fa-plus-circle {
     color: green;
     font-size: 100px;
+    margin-top: 200px;
   }
 `;
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isShowing: false
+    };
+  }
+  open() {
+    this.setState({
+      isShowing: true
+    });
+  }
+  onSubmit(e) {
+    e.prevetDefault();
+  }
+
   render() {
     return (
       <HomeWrapper>
-        <div className="add-button">
+        <div className="add-button" onSubmit={e => e}>
           <span className="fas fa-plus-circle" />
+          <span className="add-oil">Add Oil</span>
         </div>
+        <OilList />
       </HomeWrapper>
     );
   }
