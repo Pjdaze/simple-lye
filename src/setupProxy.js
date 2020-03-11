0323cd34596027d92cd6b9e0d4d07a326b986d2a
Pjdaze <pjdazeux@gmail.com>
2020-03-10 20:42:22 -0400

    stable but proxy not working

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-module.exports = function(app) {
+export default function(app) {
   app.use(
     '/sap-values',
     createProxyMiddleware({
@@ -9,4 +9,4 @@ module.exports = function(app) {
       secure: false
     })
   );
-};
+}
